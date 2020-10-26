cc5a8f4d76c23112d86c8587a006088cb246a1b6
syl3 <syle3@richplanet.kr>
2020-10-26 14:17:10 +0900

    set

--- src/api/event.ts
+++ src/api/event.ts
@@ -4,7 +4,7 @@ import { ApiDataType } from '../types/api';
 
 
 const headers = {
-    Authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c24iOjEwMDAxMjQsInJvbGVzIjpbeyJhdXRob3JpdHkiOiJST0xFX1UifV0sImlzcyI6InJpY2hwbGFuZXQuY28ua3IiLCJ1c2VyVHlwZSI6MCwiZXhwIjoxNjM0NzAzNTMzLCJ1c2VySWQiOiJjb3JpdGVzdDAwMDFAcmljaHBsYW5ldC5rciIsImlhdCI6MTYwMzE2NzUzM30.6SScnkPiOG3l0mD9ZxQQvsysR_wEJ3yH25jPUt0h0s0C2RdBRh0-ljR6o2puLBfUa9ECXOWRMSUyVyhlCiIq3A',
+    Authorization: '',
 }
 
 export function getReply(params: ApiDataType) {
